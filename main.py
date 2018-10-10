8f86d5b07770397d9be302ade7f5fd803ad99a39
Jonah Martinez <Jonah.Martinez01@outlook.com>
2018-10-10 19:19:40 -0400

    Finalized build-a-blog

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
